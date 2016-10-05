e5c5340ce19f33630e3b1e5d4e64d956a0d1a1c0
BlackHorne <black.hornetnikita@gmail.com>
2016-10-05 15:57:12 +0300

    Вторая попытка

--- paradigms_homework2.py
+++ paradigms_homework2.py
@@ -1,21 +1,20 @@
 import os
 import sys
 import hashlib
-
 from collections import defaultdict
-hashsum_and_filenames = defaultdict(list)
-hashsum_and_filenames_path = defaultdict(list)
+
+hash_to_files = defaultdict(list)
 base_path = sys.argv[1]
-for actual_direct, include_direct, files in os.walk(base_path):
+for current_dir, _, files in os.walk(base_path):
     for filename in files:
-        filename_path = os.path.join(actual_direct, filename)
-        if not (os.path.islink(filename_path) or (filename[0] == '.') or (filename[0] == '~')):
-            symvols = hashlib.md5()
-            with open(filename_path, 'rb') as file:
-                symvols.update(file.read())
-            filename_hashsum = symvols.hexdigest()
-            hashsum_and_filenames[filename_hashsum].append(filename)
-            hashsum_and_filenames_path[filename_hashsum].append(filename_path)
-for iterator in hashsum_and_filenames:
-    if len(hashsum_and_filenames[iterator]) > 1:
-        print(':' .join(hashsum_and_filenames_path[iterator]))
+        files_paths = os.path.join(current_dir, filename)
+        if not (os.path.islink(files_paths) or
+                filename.startswith(('.', '~'))):
+            hash = hashlib.md5()
+            with open(files_paths, 'rb') as file:
+                hash.update(file.read())
+            filename_hash = hash.hexdigest()
+            hash_to_files[filename_hash].append(os.path.abspath(files_paths))
+for hash_to_values in hash_to_files.values():
+    if len(hash_to_values) > 1:
+        print(':'.join(hash_to_values))
