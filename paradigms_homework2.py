58eb3ea79327a220f290c623efae61044c4fe408
BlackHorne <black.hornetnikita@gmail.com>
2016-10-06 02:09:01 +0300

    Третья попытка

--- paradigms_homework2.py
+++ paradigms_homework2.py
@@ -7,14 +7,14 @@ hash_to_files = defaultdict(list)
 base_path = sys.argv[1]
 for current_dir, _, files in os.walk(base_path):
     for filename in files:
-        files_paths = os.path.join(current_dir, filename)
-        if not (os.path.islink(files_paths) or
-                filename.startswith(('.', '~'))):
+        path = os.path.join(current_dir, filename)
+        if (not os.path.islink(path) and
+                not filename.startswith('.', '~')):
             hash = hashlib.md5()
-            with open(files_paths, 'rb') as file:
+            with open(path, 'rb') as file:
                 hash.update(file.read())
-            filename_hash = hash.hexdigest()
-            hash_to_files[filename_hash].append(os.path.abspath(files_paths))
-for hash_to_values in hash_to_files.values():
-    if len(hash_to_values) > 1:
-        print(':'.join(hash_to_values))
+            file_hash = hash.hexdigest()
+            hash_to_files[file_hash].append(os.path.abspath(files_paths))
+for hash_to_file in hash_to_files.values():
+    if len(hash_to_file) > 1:
+        print(':'.join(hash_to_file))
