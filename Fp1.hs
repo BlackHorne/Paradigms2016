bb0fc0eeaca76e84967afd6408d4f422c7503469
BlackHorne <black.hornetnikita@gmail.com>
2016-11-21 07:16:10 +0300

    Вторая попытка

--- Fp1.hs
+++ Fp1.hs
@@ -1,25 +1,28 @@
-head' (x:xs) = x
+head' (x:_) = x
 
-tail' (x:xs) = xs
+tail' (_:xs) = xs
 tail' _ = [] 
 
 take' 0 _ = []
 take' n (x:xs) = x : take' (n - 1) xs
 
 drop' 0 x = x
-drop' n (x:xs) = drop' (n - 1) xs
+drop' n (_:xs) = drop' (n - 1) xs
 drop' n _ = []
 
 filter' f (x:xs) | f x = x : filter' f xs
                  | otherwise = filter' f xs
-filter' f _ = []
+filter' _ _ = []
 
 foldl' f z (x:xs) = foldl' f (f z x) xs
-foldl' f z _ = z
+foldl' _ z _ = z
 
 concat' (x:xs) y = x : concat' xs y
 concat' _ y = y
 
-quickSort' (x:xs) = concat' (concat'  (quickSort' (filter' (<x) (x:xs))) (filter' (==x) (x:xs))) (quickSort' (filter' (>x) (x:xs)))
+quickSort' (x:xs) = concat' (concat' less equal) more
+	where
+		less = quickSort' (filter' (<x) (x:xs))
+	        more = quickSort' (filter' (>x) (x:xs))
+		equal = filter' (==x) (x:xs)
 quickSort' x = x
-
