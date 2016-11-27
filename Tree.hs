58affc0a88fc981fa2ba43583e15e0139b4c9fce
BlackHorne <black.hornetnikita@gmail.com>
2016-11-27 22:37:36 +0300

    Вторая попытка

--- Tree.hs
+++ Tree.hs
@@ -1,37 +1,39 @@
-data BinaryTree = Nil | Node (Int, Int) BinaryTree BinaryTree deriving (Show, Eq)
+import Prelude hiding (lookup)
 
-data GetResult v = NotFound | Found v deriving Show
+data BinaryTree k v = Nil | Node (k, v) (BinaryTree k v) (BinaryTree k v) deriving Show
 
-lookup k (Node (k', v) leftTree rightTree) 
-                |k == k' = Found v
-                |k > k' = lookup k rightTree
-                |k < k' = lookup k leftTree
-lookup _ _ = NotFound
+lookup :: Ord k => k -> BinaryTree k v -> Maybe v
+lookup k (Node (k', v) leftTree rightTree)
+                |k == k' = Just v
+                |k > k'  = lookup k rightTree
+                |k < k'  = lookup k leftTree
+lookup _ _ = Nothing
 
+insert :: Ord k => k -> v -> BinaryTree k v -> BinaryTree k v
 insert k v (Node (k', v') leftTree rightTree)
-                |k > k' = Node (k', v') leftTree (insert k v rightTree)
-                |k < k' = Node (k', v') (insert k v leftTree) rightTree
+                |k > k'  = Node (k', v') leftTree (insert k v rightTree)
+                |k < k'  = Node (k', v') (insert k v leftTree) rightTree
                 |k == k' = Node (k, v) leftTree rightTree
 insert k v _ = Node (k, v) Nil Nil
 
+delete :: Ord k => k -> BinaryTree k v -> BinaryTree k v
 delete k (Node (k', v) leftTree rightTree)
                 |k > k' = Node (k', v) leftTree (delete k rightTree)
                 |k < k' = Node (k', v) (delete k leftTree) rightTree
-                |k == k' = mergeHeap
-                        where mergeHeap 
-                                |leftTree == Nil = rightTree 
-                                |rightTree == Nil = leftTree
-                                |otherwise = replaceMin (Node (k', v) leftTree rightTree)
+                |isEmpty leftTree  = rightTree 
+                |isEmpty rightTree = leftTree
+                |otherwise = (Node minPair leftTree (delete minKey rightTree))
+                        where 
+                                minPair = findMinPair rightTree
+                                minKey  = findMinKey rightTree
+
+isEmpty (Node (k, v) leftTree rightTree) = False
+isEmpty Nil = True
 
 findMinPair (Node (k, v) leftTree _) 
-                |leftTree == Nil = (k, v)
+                |isEmpty leftTree = (k, v)
                 |otherwise = findMinPair leftTree
 
 findMinKey (Node (k, _) leftTree _) 
-                |leftTree == Nil = k
+                |isEmpty leftTree = k
                 |otherwise = findMinKey leftTree
-
-replaceMin (Node (_, _) leftTree rightTree) = Node minPair leftTree (delete minKey rightTree)
-                where 
-                        minPair = findMinPair rightTree
-                        minKey = findMinKey rightTree
